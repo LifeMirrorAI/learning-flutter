5ad0e3f0980e275bc43f644cb2ef566c236dd234
jacques-swanepoel <jacques@swanepoel.ca>
2024-06-30 11:23:11 -0400

    04_02_b done

--- chat_app/lib/demo/counter_stateful_demo.dart
+++ chat_app/lib/demo/counter_stateful_demo.dart
@@ -1,2 +1,41 @@
 
 //TODO: Recreate the counter app
+
+import 'package:flutter/material.dart';
+
+class CounterStateful extends StatefulWidget {
+  const CounterStateful({Key? key}) : super(key: key);
+
+  @override
+  State<CounterStateful> createState() {
+    State<CounterStateful> stateClassAssociatedWithThisWidget =
+    _CounterStatefulState();
+    return stateClassAssociatedWithThisWidget;
+  }
+}
+
+class _CounterStatefulState extends State<CounterStateful> {
+  int counter = 0;
+  void increment() {
+    setState(() {
+      counter++;
+    });
+    print(counter);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+        backgroundColor: Colors.green
+      ),
+        body: Center(
+          child: Text('$counter', style: TextStyle(fontSize: 30),
+          ),
+        )
+    );
+  }
+}
